4fb9d45659b5bb4d807760557228c8ebb4cd5715
katio <onokatio@gmail.com>
2019-07-17 16:14:53 +0900

    finish

--- parser_expression_1i.c
+++ parser_expression_1i.c
@@ -20,26 +20,35 @@ parser_init(void)
 	nextsym = scanner_get_next_sym();
 }
 
-/* E ::= T { + T } ; */
+/* E ::= T { ( + | - ) T } ; */
 int
 parser_parse_expression(void)
 {
-	int		r;
+	int		r = 0;
+	int 	s;
+	int 	mp_value = 1; // 1 == plus, -1 == minus
 
-	r = parse_term();
-	while (nextsym.sym != SYM_SEMICOLON){
+	while (nextsym.sym != SYM_SEMICOLON && nextsym.sym != SYM_RPAREN){
 		if (nextsym.sym == SYM_PLUS){
 			nextsym = scanner_get_next_sym();
+		} else if (nextsym.sym == SYM_MINUS){
+			nextsym = scanner_get_next_sym();
+			mp_value *= -1;
 		}else{
-			ERROR("'+' が必要です");
+			s = parse_term();
+			while (nextsym.sym == SYM_ASTERISK || nextsym.sym == SYM_SLASH  || nextsym.sym == SYM_CONSTANT_INT ){
+				if (nextsym.sym == SYM_ASTERISK){
+					nextsym = scanner_get_next_sym();
+					s *= parse_term();
+				} else if (nextsym.sym == SYM_SLASH){
+					nextsym = scanner_get_next_sym();
+					s /= parse_term();
+				}
+			}
+			r += mp_value * s;
+			mp_value = 1;
 		}
-		r += parse_term();
 	}
-	if (nextsym.sym != SYM_SEMICOLON){
-		ERROR("';' が必要です");
-	}
-	/* ';' は式の終わりの記号なので，それ以上先読みをしない */
-	/*nextsym = scanner_get_next_sym();*/
 
 	return (r);
 }
@@ -61,6 +70,10 @@ parse_number(void)
 
 	if (nextsym.sym == SYM_CONSTANT_INT){
 		r = nextsym.integer;
+		nextsym = scanner_get_next_sym();
+	}else if (nextsym.sym == SYM_LPAREN){
+		nextsym = scanner_get_next_sym();
+		r = parser_parse_expression();
 		nextsym = scanner_get_next_sym();
 	}else{
 		ERROR("<N> が必要です");
